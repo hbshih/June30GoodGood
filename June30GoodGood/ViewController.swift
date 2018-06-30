31c6fc9798f692c9bb12d84df6cbd5126f82e5f4
“hbshih” <hbshih@gmail.com>
2018-06-30 11:28:43 +0800

    Initial Commit

---
+++ June30GoodGood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  June30GoodGood
+//
+//  Created by Ben Shih on 30/06/2018.
+//  Copyright © 2018 BenShih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
